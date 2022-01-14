dbd830bfc4a8277857dba97a911e513951df4e88
mecseridavid <mecseri.david.mark@students.jedlik.eu>
2022-01-14 21:53:49 +0100

    some more tests

---
+++ src/post/__tests__/post.test.ts
@@ -0,0 +1,29 @@
+import "dotenv/config";
+import App from "../../app";
+import AuthenticationController from "../../authentication/authentication.controller";
+import PostController from "../../post/post.controller";
+import validateEnv from "../../utils/validateEnv";
+import * as request from "supertest";
+
+validateEnv();
+
+let server: Express.Application;
+// eslint-disable-next-line @typescript-eslint/ban-types
+let cookie: string | any;
+
+beforeAll(async () => {
+    server = new App([new AuthenticationController(), new PostController()]).getServer();
+    const response = await request(server).post("/auth/login").send({
+        email: "student001@jedlik.eu",
+        password: "student001",
+    });
+    cookie = response.headers["set-cookie"];
+});
+
+describe("test API endpoints", () => {
+    it("GET /posts", async () => {
+        const response = await request(server).get("/posts").set("Cookie", cookie);
+        const count: unknown = response.body.count;
+        expect(response.body.count).toBe(count);
+    });
+});
--- src/recipe/__tests__/recipe.test.ts
+++ src/recipe/__tests__/recipe.test.ts
@@ -21,23 +21,75 @@ beforeAll(async () => {
 });
 
 describe("test recipes endpoints", () => {
+    let id: string, count: number;
+
     it("GET /recipes", async () => {
         const response = await request(server).get("/recipes").set("Cookie", cookie);
-        const count = response.body.count;
+        count = response.body.count;
         expect(response.statusCode).toEqual(200);
         expect(response.body.count).toEqual(count); // basically 10
     });
 
+    it("GET /recipes (search for 'keyword')", async () => {
+        const response = await request(server).get("/recipes/0/5/discription/1/paradicsom").set("Cookie", cookie);
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.count).toBeLessThan(count);
+    });
+
+    it("GET /recipes (search for wrong 'keyword')", async () => {
+        const response = await request(server).get("/recipes/0/5/discription/1/goesiéhgesouihg").set("Cookie", cookie);
+        expect(response.statusCode).toEqual(200);
+        expect(response.body.count).toEqual(0);
+    });
+
     it("GET /recipes/:id  (wrong id)", async () => {
-        const id = "uogawagfhianwoigaahnwg";
+        id = "uogawagfhianwoigaahnwg";
         const response = await request(server).get(`/recipes/${id}`).set("Cookie", cookie);
         expect(response.statusCode).toEqual(404);
         expect(response.body.message).toEqual(`This ${id} id is not valid.`);
     });
 
-    it("POST /recipes", async () => {
+    it("DELETE /recipes/:id  (wrong id)", async () => {
+        const response = await request(server).delete(`/recipes/${id}`).set("Cookie", cookie);
+        expect(response.statusCode).toEqual(404);
+        expect(response.body.message).toEqual(`This ${id} id is not valid.`);
+    });
+
+    it("PATCH /recipes/:id  (wrong id)", async () => {
+        const response = await request(server).patch(`/recipes/${id}`).set("Cookie", cookie);
+        expect(response.statusCode).toEqual(404);
+        expect(response.body.message).toEqual(`This ${id} id is not valid.`);
+    });
+
+    it("POST /recipes (empty)", async () => {
         const response = await request(server).post("/recipes").set("Cookie", cookie);
         expect(response.statusCode).toEqual(400);
         expect(response.body.message).toEqual("recipeName must be a string,recipeName should not be empty, imageURL must be a string,imageURL must be an URL address,imageURL should not be empty, description must be a string,description should not be empty, ingredients should not be empty,ingredients must be an array");
+    });
+
+    it("POST /recipes", async () => {
+        const response = await request(server)
+            .post("/recipes")
+            .set("Cookie", cookie)
+            .send({
+                recipeName: "asd",
+                imageURL: "www.fgoiawf.hu",
+                description: "asd",
+                ingredients: ["asd"],
+            });
+        id = response.body._id;
+        expect(response.statusCode).toEqual(200);
+    });
+
+    it("PATCH /recipes/:id", async () => {
+        const response = await request(server).patch(`/recipes/${id}`).set("Cookie", cookie).send({
+            recipeName: "asdasd",
+        });
+        expect(response.statusCode).toEqual(200);
+    });
+
+    it("DELETE /recipes/:id", async () => {
+        const response = await request(server).delete(`/recipes/${id}`).set("Cookie", cookie);
+        expect(response.statusCode).toEqual(200);
     });
 });
